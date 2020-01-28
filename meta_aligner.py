6f0b35320568c5900cbdba4363d32b5bc80a5a8f
Pavan Mandava <mspavan04@gmail.com>
2020-01-28 16:00:19 +0100

    Isaac: Master Align file fixes

--- meta_aligner.py
+++ meta_aligner.py
@@ -10,22 +10,22 @@ def zip_alignments(al0lang0, al0lang1, al1lang0, al1lang1):
         new1 = {}
         new2 = {}
         k, k0, k1 = 0, 0, 0
-        while k0<len(al0lang0) and k1<len(al0lang):
-            if v0==al1lang0[k0]:
-                new0.update({k:v0})
+        while k0<len(al0lang0) and k1<len(al1lang0):
+            if al0lang0[k0]==al1lang0[k0]:
+                new0.update({k:al0lang0[k0]})
                 new1.update({k:al0lang1[k0]})
                 new2.update({k:al1lang1[k1]})
                 k0+=1
                 k1+=1
-            elif v0==+al1lang0[k1]+' '+al1lang0.get(k1+1, '')
-                new0.update({k:v0})
+            elif al0lang0[k0]==+al1lang0[k1]+' '+al1lang0.get(k1+1, ''):
+                new0.update({k:al0lang0[k0]})
                 new1.update({k:al0lang1[k0]})
-                new2.update({k:al1lang1[k1]+' '+al1lang0.get(0k1+1, '')})
+                new2.update({k:al1lang1[k1]+' '+al1lang0.get(k1+1, '')})
                 k0+=1
                 k1+=2
-            elif v0+al0lang0.get(k0+1, '')==al1lang0[k1]:
-                new0.update({k:k:al0lang0[k0]+' '+al0lang0.get(k0+1, '')})
-                new1.update({k:k:al0lang1[k0]+' '+al0lang1.get(k0+1, '')})
+            elif al0lang0[k0]+al0lang0.get(k0+1, '')==al1lang0[k1]:
+                new0.update({k:al0lang0[k0]+' '+al0lang0.get(k0+1, '')})
+                new1.update({k:al0lang1[k0]+' '+al0lang1.get(k0+1, '')})
                 new2.update({k:al1lang1[k1]})
                 k0+=2
                 k1+=1
